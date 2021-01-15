d0ccbe730e4282829876e8edc343512da634c667
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2021-01-15 10:00:21 +0100

    initial setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='easylabwork',
+    version='0.1.0',
+    author='Jeremy Fix',
+    author_email='jeremy.fix@centralesupelec.fr',
+    packages=['easylabwork'],
+    license='LICENSE',
+    description='Python package from making it easy to generate lab work codes',
+    entry_points={
+        'console_scripts': ['easylabwork=easylabwork.easylabwork:main']
+    }
+)
